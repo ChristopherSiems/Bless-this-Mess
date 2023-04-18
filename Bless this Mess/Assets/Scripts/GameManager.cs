1bdd2a3caf165432aaba761615cf01914d0b9fea
gitopher <siemsck@gmail.com>
2023-04-18 12:27:48 -0400

    timer

--- Bless this Mess/Assets/Scripts/GameManager.cs
+++ Bless this Mess/Assets/Scripts/GameManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour{
+    public TMP_Text clock;
     public float timer = 0;
     public float limit;
+    private int min;
+    private int sec;
+    private float diff;
 
     // Start is called before the first frame update
     void Start(){
@@ -13,6 +18,15 @@ public class GameManager : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         timer += Time.deltaTime;
+        diff = limit - timer;
+        min = (int)diff / 60;
+        sec = (int)diff - (min * 60);
+        if (sec > 10){
+            clock.text = $"{min}:{sec}";
+        }
+        else{
+            clock.text = $"{min}:0{sec}";
+        }
         /*if (timer >= limit){
 
         }*/
