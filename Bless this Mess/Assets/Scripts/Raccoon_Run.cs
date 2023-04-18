5eb970bf940e61164e67ae4f990eff939daad387
Gigex15 <129200306+Gigex15@users.noreply.github.com>
2023-04-18 10:11:46 -0400

    Part of Start Screen and Beginnings of Raccoon AI

---
+++ Bless this Mess/Assets/Scripts/Raccoon_Run.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raccoon_Run : StateMachineBehaviour
+{
+    public float speed = 2.5f;
+    
+    Transform player;
+    Rigidbody2D rb;
+    RaccoonGeneral boss;
+
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<RaccoonGeneral>();
+    }
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss.LookAtPlayer();
+
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+    }
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
